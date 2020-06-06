fa3c30f17da2f2e86cdc8183dd6c8433bd4de300
marina <marinamsm@live.com>
2020-06-06 15:54:40 -0300

    IP workaround for mobile

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -3,12 +3,13 @@ import { Request, Response } from "express";
 
 class ItemsCtrl {
   async index(request: Request, response: Response) {
+    const IPAddress = '192.168.0.10';
     const items = await knex("items").select("*");
     const serializedItems = items.map((item) => {
       return {
         id: item.id,
         title: item.title,
-        imageUrl: `http://localhost:3333/uploads/${item.image}`,
+        imageUrl: `http://${IPAddress}:3333/uploads/${item.image}`,
       };
     });
     response.json(serializedItems);
